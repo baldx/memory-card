7dca602176e91ea6211db4d98f2d83a7167be116
baldx <mr.ahmad.m2006@gmail.com>
2024-02-11 17:12:19 +0100

    add header & card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './components/header'
+import OnLoad from './components/card'
 
 function App() {
 
   return (
     <>
-      
+      <Header score={0} best={0} />
+      <main>
+        <OnLoad />
+      </main>
     </>
   )
 }
