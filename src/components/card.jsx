dd1916fdcccb7c2e08ff61bf0a70db547ea24e91
baldx <mr.ahmad.m2006@gmail.com>
2024-02-11 17:12:49 +0100

    add card skeleton

---
+++ src/components/card.jsx
@@ -0,0 +1,11 @@
+export default function OnLoad() {
+    
+    return (
+        <>
+        <div className="card">
+            <img src="src/assets/flashsuit.jpg" alt="" />
+            <div className="name">Flash</div>
+        </div>
+        </>
+    )
+}
