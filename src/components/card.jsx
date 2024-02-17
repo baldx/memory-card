9cc7891055759eb3ca603e7ff7dc15bc0e6f9171
baldx <mr.ahmad.m2006@gmail.com>
2024-02-17 20:54:36 +0100

    create general component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,11 @@
-export default function OnLoad() {
+// eslint-disable-next-line react/prop-types
+export default function Card({src, name}) {
     
     return (
         <>
         <div className="card">
-            <img src="src/assets/flashsuit.jpg" alt="" />
-            <div className="name">Flash</div>
+            <img src={src} alt="" />
+            <div className="name">{name}</div>
         </div>
         </>
     )
