173ab9da0adf12ca7aba5007e7edee6afbaff712
baldx <mr.ahmad.m2006@gmail.com>
2024-02-17 22:33:32 +0100

    add all cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Card from './components/card'
 
 function App() {
 
+  /*write random gen function here*/
+
   return (
     <>
       <Header score={0} best={0} />
       <main>
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
-        <Card src={"src/assets/flashsuit.jpg"} name="Flash" />
+        <Card search="barry allen" name="Flash" />
+        <Card search="iris west" name="Iris West" />
+        <Card search="killer frost" name="Caitlin Snow" />
+        <Card search="Joe West" name="Joe West" />
+        <Card search="oliver queen" name="Green Arrow" />
+        <Card search="cisco ramon" name="Vibe" />
+        <Card search="Wentworth Miller" name="Captain Cold" />
+        <Card search="batman" name="Batman" />
+        <Card search="super girl" name="Super Girl" />
+        <Card search="dc legends of tomorrow" name="Legends" />
       </main>
     </>
   )
